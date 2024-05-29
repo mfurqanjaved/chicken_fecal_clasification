ccaf4215f341b4f8a32fa9fe94bd794dcf5b63ad
mfurqanjaved <mfurqanjaved@gmail.com>
2024-05-29 18:12:00 +0500

    user app added

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -11,7 +11,7 @@ class PredictionPipeline:
 
 
     def predict(self):
-        model = load_model(os.path.join("artifacts","trzining","model.h5"))
+        model = load_model(os.path.join("artifacts","training","model.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size=(224, 224))
